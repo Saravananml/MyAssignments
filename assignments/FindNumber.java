057a4cca869f8a3c8dcbc17f88498a43fda947cb
Saravananml <147952289+Saravananml@users.noreply.github.com>
2023-10-14 18:26:52 -0400

    Add files via upload
    Day1Assignments

---
+++ assignments/FindNumber.java
@@ -0,0 +1,19 @@
+package week1.day1.assignments;
+
+public class FindNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 45;
+		if (a>0)
+		{
+			System.out.println("The given number is positive");
+		}
+		else
+		{
+			System.out.println("The given number is negative");
+		}
+	}
+	
+
+}
---
+++ assignments/Firefox.java
@@ -0,0 +1,18 @@
+package week1.day1.assignments;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float browserVersion = 100.2f;
+		String browserName = "firefox";
+		boolean pageLoaded = true;
+		int noOfBrowser = 9;
+		char browserLogo = 'f';
+		System.out.println("The browser version is: " +  browserVersion);
+		System.out.println("The browser name is: " + browserName);
+		System.out.println("Is the page loaded: " + pageLoaded);
+		System.out.println("No of browsers: " +  noOfBrowser);
+		System.out.println("What is the browsor logo: " + browserLogo);
+	}
+}
